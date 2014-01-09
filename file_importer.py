dcf7bd73ccc2f871ab8d48c43d11a8e5b392b6de
mcehlert <mathias.ehlert@gmail.com>
2014-01-09 16:50:53 +0100

    a lot of refactoring for more modular structure

--- file_importer.py
+++ file_importer.py
@@ -1,28 +1,26 @@
 import networkx as nx
-import redis as rd
 
 class FileImporter(object):
+
   def __init__(self,filename):
+    # initialize data file to parse and new empty graph
+
     self.data_file  = open(filename)
-    self.all_nodes  = []
-    self.redis      = rd.StrictRedis(host='localhost', port=6379, db=0)
     self.graph      = nx.Graph()
 
   def read(self):
-    self.redis.flushdb()
     for line in self.data_file:
       self.parse_line(line)
-    self.save_all_nodes()
     return self.graph
 
   def parse_line(self, line):
+    # split each line on tabstop
+    # first field specifies the source node
+    # second field specifies the target node
+
     fields = line.strip().split("\t")
     from_node = int(fields[0])
     to_node = int(fields[1])
-    self.all_nodes.extend([from_node,to_node])
-    self.graph.add_edge(from_node, to_node)
 
-  def save_all_nodes(self):
-    self.unique_nodes = list(set(self.all_nodes))
-    self.unique_nodes.sort()
-    self.redis.sadd('all_nodes', *self.unique_nodes)
+    # add edge to the graph
+    self.graph.add_edge(from_node, to_node)
