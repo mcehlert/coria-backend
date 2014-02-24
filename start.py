634b3664b6b694303fb9787781e074432967ead5
mcehlert <mathias.ehlert@gmail.com>
2014-02-24 16:45:06 +0100

    added profiling option to start script

--- start.py
+++ start.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
+import datetime
 import argparse
+import cProfile, pstats, StringIO
 from file_importer import FileImporter
 from metric_calculator import MetricCalculator
 
@@ -8,9 +10,23 @@ parser = argparse.ArgumentParser(description='Read a Tab-separated Graph Datafil
 parser.add_argument('filename', metavar='filename', type=str,
                    help='the name of the data file containing tab separated node ids')
 
+parser.add_argument('--profiling',dest='profiling',action='store_true', help='enable runtime profiling into profiling.txt file')
+
 args = parser.parse_args()
+
+if args.profiling:
+  pr = cProfile.Profile()
+  s = StringIO.StringIO()
+  timestamp = str(datetime.datetime.now().strftime('%Y%m%d%H%M%S'))
+  outfile = open('profiling_output_'+timestamp+'.txt', 'w')
+  pr.enable()
 
 fi = FileImporter(args.filename)
 graph = fi.read()
 mc = MetricCalculator(graph)
 mc.start()
+
+if args.profiling:
+  ps = pstats.Stats(pr, stream=s).sort_stats('cumulative')
+  ps.print_stats()
+  outfile.write(s.getvalue())
